f5da4d9cbf296a5e0ec0d29be0ebab07c15031f2
Edson Alencar <edson.alencar@lais.huol.ufrn.br>
2022-09-18 22:27:54 -0300

    add: entrada do nome do arquivo pelo terminal

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,37 @@
 import * as fs from "node:fs";
+import { createInterface } from "node:readline";
+import readline from "readline";
 import { MipsAssembler } from "./Mips";
 import { mipsType } from "./utils/checkMipsType";
 
-try {
-  const data = fs.readFileSync("assembly.txt");
-  const mips = new MipsAssembler(data);
+const scanner = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-  mips.readRows.forEach((row) => {
-    console.log(mipsType(row));
-  });
+// scanner.question("Digite o nome do arquivo: ", (input) => {
+//   let fileName = "";
 
-  //   console.log(mips.labels);
-} catch (error) {
-  console.log(error);
+//   if (input.includes(".asm")) fileName = input;
+//   else fileName = input + ".asm";
+
+//   main(fileName);
+//   scanner.close();
+// });
+
+main("assembly.asm");
+
+function main(fileName: string) {
+  try {
+    const data = fs.readFileSync(fileName);
+    const mips = new MipsAssembler(data);
+
+    mips.readRows.forEach((row) => {
+      console.log(mipsType(row));
+    });
+
+    // console.log(mips.labels);
+  } catch (error) {
+    console.log(error);
+  }
 }
